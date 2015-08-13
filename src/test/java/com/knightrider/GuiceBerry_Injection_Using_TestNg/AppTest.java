cda4b76060a8abca469a0dd70d3774acc9b8d2a1
Prashant Kumar <Prashant3.kumar@one97.net>
2015-08-13 17:51:05 +0530

    Modified README.md

--- src/test/java/com/knightrider/GuiceBerry_Injection_Using_TestNg/AppTest.java
+++ src/test/java/com/knightrider/GuiceBerry_Injection_Using_TestNg/AppTest.java
@@ -12,7 +12,7 @@ public class AppTest
 {
     /**
      * Create the test case
-     * @param canary test
+     * @param Canary test
      * @param testName name of the test case
      */
     public AppTest( String testName )
